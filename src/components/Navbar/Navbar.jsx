56adf427d5da28d08968ab105a8559f394126a24
KAndrade-SP <kaique.viana090@gmail.com>
2023-08-22 18:41:37 -0300

    Exibição dos dados de usuários na Home após Login

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,52 +1,122 @@
-import React, { useState } from 'react'
-import { List, Lock, X } from "@phosphor-icons/react"
+import React, { useState, useEffect } from 'react'
+import { List, X, House, GraduationCap, Question, Info, PaperPlaneTilt, SignOut } from "@phosphor-icons/react"
+
+import { auth } from '../../services/firebase'
 
 import './styles.scss'
 
 const Navbar = () => {
 
-    const [toggle,setToggle]=useState(false)
-    const handleClick = ()=> setToggle(!toggle)
+    const [currentUser, setCurrentUser] = useState()
+    const [isLogged, setLogged] = useState(false)
+    const [toggle,setToggle] = useState(false)
+    const handleClick = () => setToggle(!toggle)
 
-  return (
-    <div className='w-full h-[80px] border-b'>
-        <div className='max-w-[1024px] mx-5 lg:m-auto w-full h-full flex justify-between items-center md:px-0 px-4'> 
-            
-            <p className='text-lg'>DevY</p>
-            
-            <div className='hidden md:flex items-center '>
-                <ul className='flex gap-4'>
-                    <li className='nav-link'>Início</li>
-                    <li className='nav-link'>Cursos</li>
-                    <li className='nav-link'>Ajuda</li>
-                    <li className='nav-link'>Sobre</li>
-                    <li className='nav-link'>Contatos</li>
+    useEffect(() => {
+        let isMounted = true
+
+        auth.onAuthStateChanged(user => {
+            if (isMounted && user) { console.log(user) }
+            setLogged(true)
+            setCurrentUser(user)
+        })
+
+        return () => { isMounted = false }
+    }, [])  
+
+    return (
+        <div className='w-full h-[80px] border-b'>
+            <div className='max-w-[1024px] mx-5 lg:m-auto w-full h-full flex justify-between items-center md:px-0 px-4'> 
+                
+                <p className='text-lg'>DevY</p>
+                
+                <div className='hidden md:flex items-center '>
+                    <ul className='flex gap-4'>
+                        <li className='nav-link'>Início</li>
+                        <li className='nav-link'>Cursos</li>
+                        <li className='nav-link'>Ajuda</li>
+                        <li className='nav-link'>Sobre</li>
+                        <li className='nav-link'>Contatos</li>
+                    </ul>
+                </div>
+
+                <div className='hidden md:flex'>
+
+                    {
+                    isLogged && currentUser ?
+                        <a className='flex justify-end gap-4 items-center cursor-pointer hover:text-[#a0a2ab]'>
+                            <p className='lg:m-auto md:text-sm'>{currentUser.displayName}</p>
+                            <img src={currentUser.photoURL} className='w-[40px] mr-10 lg:m-auto rounded-full'></img>
+                        </a>
+                    :
+                        <button className='px-8 mr-10 lg:m-auto nav-button'><a href='/login'>Fazer login</a></button>
+                    }
+                    
+                </div>
+
+                <div className='md:hidden mr-10' onClick={handleClick}>
+                    {toggle?<X size={24}/>:<List size={24}/>}
+                </div>
+            </div>
+
+            <div className={toggle?'absolute z-10 w-full px-4 md:hidden':'hidden'}>
+                <ul>
+                    {
+                    isLogged && currentUser ?
+                        <a className='flex pl-4 pt-2 items-center'>
+                            <img src={currentUser.photoURL} className='w-[24px] rounded-full'></img>
+                            <li className='p-4 nav-link'>Perfil</li>
+                        </a>
+                    :
+                        <></>
+                    }
+                    <div className='flex pl-4 pt-2 items-center'>
+                        <House size={24}/>
+                        <li className='p-4 nav-link'>Início</li>
+                    </div>
+                    
+                    <div className='flex pl-4 pt-2 items-center'>
+                        <GraduationCap size={24}/>
+                        <li className='p-4 nav-link'>Cursos</li>
+                    </div>
+                    
+                    <div className='flex pl-4 pt-2 items-center'>
+                        <Question size={24}/>
+                        <li className='p-4 nav-link'>Ajuda</li>
+                    </div>
+                    
+                    <div className='flex pl-4 pt-2 items-center'>
+                        <Info size={24}/>
+                        <li className='p-4 nav-link'>Sobre</li>
+                    </div>
+
+                    <div className='flex pl-4 pt-2 items-center'>
+                        <PaperPlaneTilt size={24}/>
+                        <li className='p-4 nav-link'>Contatos</li>
+                    </div>
+                    
+                    {
+                    isLogged && currentUser ?
+                        <div className='flex pl-4 pt-2 items-center'>
+                            <SignOut size={24}/>
+                            <li className='p-4 nav-link'>Sair</li>
+                        </div>
+                    :
+                        <></>
+                    }
+                    
+                    <div className='flex flex-col my-2 gap-4'>
+                        {
+                        isLogged && currentUser ?
+                            <></>
+                        :
+                            <button className='px-8 nav-button'><a href='/login'>Fazer login</a></button>
+                        }
+                    </div>
                 </ul>
             </div>
-
-            <div className='hidden md:flex'>
-                <button className='px-8 mr-10 lg:m-auto nav-button'>Fazer login</button>
-            </div>
-
-            <div className='md:hidden mr-10' onClick={handleClick}>
-                {toggle?<X size={24}/>:<List size={24}/>}
-            </div>
         </div>
-
-        <div className={toggle?'absolute z-10 w-full px-8 md:hidden':'hidden'}>
-            <ul>
-                <li className='p-4 nav-link'>Início</li>
-                <li className='p-4 nav-link'>Cursos</li>
-                <li className='p-4 nav-link'>Ajuda</li>
-                <li className='p-4 nav-link'>Sobre</li>
-                <li className='p-4 nav-link'>Contatos</li>
-                <div className='flex flex-col my-4 gap-4'>
-                    <button className='px-8 rounded-md nav-button'>Fazer login</button>
-                </div>
-            </ul>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Navbar
