9f190c5a84bc656b0d7e8ee50a0d12ffe37175e3
KAndrade-SP <kaique.viana090@gmail.com>
2023-11-17 22:08:30 -0300

    Remoção da firebase config (segurança)

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -2,12 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCllybWBtxKwQsEBmzqpQkfkW_C0ymlgb4",
-  authDomain: "devy-project.firebaseapp.com",
-  projectId: "devy-project",
-  storageBucket: "devy-project.appspot.com",
-  messagingSenderId: "292738799466",
-  appId: "1:292738799466:web:3e8ad8bc6ce5f22afe7713"
+
 };
 
 const app = initializeApp(firebaseConfig);
