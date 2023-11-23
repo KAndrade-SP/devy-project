aa18995ca151fee0058100efdfc5af00e539d012
KAndrade-SP <kaique.viana090@gmail.com>
2023-11-23 17:58:06 -0300

    Inserção de CSS no /create

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ import { Home } from "./pages/Home";
 
 import './styles/global.scss';
 
-
 export default function App() {
 
   const [active, setActive] = useState("home");
--- src/pages/AddEditPost/AddEditPost.jsx
+++ src/pages/AddEditPost/AddEditPost.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 
-
 import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
 import {
   addDoc,
@@ -14,6 +13,7 @@ import {
 import { toast } from "react-toastify";
 
 import { db, storage } from "../../services/firebase";
+import './styles.scss'
 
 const initialState = {
   title: "",
@@ -148,104 +148,137 @@ const AddEditPost = ({ user, setActive }) => {
   };
 
   return (
-    <section className="max-w-[1024px] mx-5 lg:m-auto w-full h-full items-center md:px-0">
-      <div className="">
-        <div className="">
-          <div className="">
-            {id ? "Atualizar post" : "Criar post"}
-          </div>
-        </div>
-        <div className="">
-          <div className="">
-            <form className="" onSubmit={handleSubmit}>
-              <div className="">
-                <input
-                  type="text"
-                  className=""
-                  placeholder="Título"
-                  name="title"
-                  value={title}
-                  onChange={handleChange}
-                />
-              </div>
-              <div className="">
-                {/* <ReactTagInput
-                  tags={tags}
-                  placeholder="Tags"
-                  onChange={handleTags}
-                /> */}
-              </div>
-              <div className="">
-                <p className="">É um trending post?</p>
-                <div className="">
+    <>
+      <section className="section">
+        <h2 className="section-title">{id ? "Atualizar post" : "Criar post"}</h2>
+      </section>
+
+      <div className="mb-2 bd-grid">
+          <form className="post__form" onSubmit={handleSubmit}>
+              <div className="post__inputs">
                   <input
-                    type="radio"
-                    className=""
-                    value="Sim"
-                    name="radioOption"
-                    checked={trending === "Sim"}
-                    onChange={handleTrending}
+                    type="text"
+                    className="post__input mb-3"
+                    placeholder="Título"
+                    name="title"
+                    value={title}
+                    onChange={handleChange}
                   />
-                  <label htmlFor="radioOption" className="">
-                    Sim&nbsp;
-                  </label>
-                  <input
-                    type="radio"
-                    className=""
-                    value="Não"
-                    name="radioOption"
-                    checked={trending === "Não"}
-                    onChange={handleTrending}
-                  />
-                  <label htmlFor="radioOption" className="">
-                    Não
-                  </label>
-                </div>
+
+                  <span className="block mb-2">É um trending post?</span>
+
+                  <div className="container">
+                    <div className="radio m-4">
+                      <input 
+                        id="radio-1" 
+                        name="radio" 
+                        type="radio" 
+                        value="Sim"
+                        checked={trending === "Sim"}
+                        onChange={handleTrending}
+                      />
+                      <label htmlFor="radio-1" className="radio-label">Sim</label>
+                    </div>
+
+                    <div className="radio m-4">
+                      <input 
+                        id="radio-2" 
+                        name="radio" 
+                        type="radio"
+                        value="Não"
+                        checked={trending === "Não"}
+                        onChange={handleTrending}
+                      />
+                      <label htmlFor="radio-2" className="radio-label">Não</label>
+                    </div>
+                  </div>
               </div>
-              <div className="">
-                <select
-                  value={category}
-                  onChange={onCategoryChange}
-                  className=""
-                >
-                  <option>Selecionar categoria</option>
-                  {categoryOption.map((option, index) => (
-                    <option value={option || ""} key={index}>
-                      {option}
-                    </option>
-                  ))}
-                </select>
-              </div>
-              <div className="">
-                <textarea
-                  className=""
-                  placeholder="Descrição"
-                  value={description}
-                  name="description"
-                  onChange={handleChange}
+
+              {/* <div className="">
+                <ReactTagInput
+                      tags={tags}
+                      placeholder="Tags"
+                      onChange={handleTags}
                 />
+              </div> */}
+
+              <select 
+                id="countries" 
+                className="post__dropdown mb-4 cursor-pointer rounded-lg block w-full p-2.5"
+                value={category}
+                onChange={onCategoryChange}
+              >
+                <option selected>Selecionar categoria</option>
+                {categoryOption.map((option, index) => (
+                  <option value={option || ""} key={index}>
+                    {option}
+                  </option>
+                ))}
+              </select>
+
+              <label 
+                htmlFor="message" 
+                className="block mb-2">
+                Descrição
+              </label>
+              <textarea 
+                id="message" 
+                rows="4" 
+                className="post__area block mb-4"
+                placeholder="Descrição da postagem"
+                value={description}
+                name="description"
+                onChange={handleChange}
+              />
+
+              <div className="flex items-center justify-center w-full">
+                  <label 
+                    htmlFor="file" 
+                    className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+                  >
+                      <div 
+                        className="flex flex-col items-center justify-center pt-5 pb-6"
+                      >
+                          <svg 
+                            className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" 
+                            aria-hidden="true" 
+                            xmlns="http://www.w3.org/2000/svg" 
+                            fill="none" 
+                            viewBox="0 0 20 16">
+                              <path 
+                                stroke="currentColor" 
+                                stroke-linecap="round" 
+                                stroke-linejoin="round" 
+                                stroke-width="2" 
+                                d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                              />
+                          </svg>
+                          <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                            <span className="font-semibold">Clique para escolher uma imagem</span> ou arraste e solte
+                          </p>
+                          <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+                      </div>
+                      <input 
+                        id="file" 
+                        type="file" 
+                        className="hidden" 
+                        onChange={(e) => setFile(e.target.files[0])}
+                      />
+                  </label>
               </div>
-              <div className="">
-                <input
-                  type="file"
-                  className=""
-                  onChange={(e) => setFile(e.target.files[0])}
-                />
-              </div>
+
               <div className="">
                 <button
                   className=""
                   type="submit"
                   disabled={progress !== null && progress < 100}
                 >
-                  {id ? "Atualizar" : "Enviar"}
+                {id ? "Atualizar" : "Enviar"}
                 </button>
               </div>
-            </form>
-          </div>
-        </div>
+          </form>
       </div>
-    </section>
+    </>
   );
 };
 
